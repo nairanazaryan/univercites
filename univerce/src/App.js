aef1d70ef7f08b6bbf8afdd17535a78193bd9684
nairanazaryan <nairanazaryan081@gmail.com>
2022-07-10 13:53:24 +0400

    finaly project

--- univerce/src/App.js
+++ univerce/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import  React,  { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state ={
+      univers: []
+    }
+    this.myRef = React.createRef('')
+  }
+  findeUniverse = () => {
+    fetch(`http://universities.hipolabs.com/search?country=${this.myRef.current.value}`)
+    .then(data => data.json())
+    // .then(res => console.log(res))
+    .then(res => this.setState({
+      univers: res
+    }))
+  }
+  render(){
+    return(
+      <section>
+        <input ref= {this.myRef}/>
+        <button onClick={this.findeUniverse}>Finde univers</button>
+        {
+          <ul>
+            {this.state.univers.map(univer =>{
+              return <li key={Math.random()} className='item'>
+                {univer.alpha_two_code}
+                <br />
+                {univer.country}
+                <br />
+                {univer.name}
+                <br />
+               {univer.web_pages}
+              </li>
+            })}
+          </ul>
+        }
+      </section>
+    )
+  }
 }
 
-export default App;
+export default App
